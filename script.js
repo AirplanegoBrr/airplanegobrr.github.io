2b2629e75155d0d951f738cf1e28d93035d31830
Glitch (midevilfractions) <none>
2020-09-30 23:20:24 +0000

    Updated 4:20PM PST 9/30/2020

--- script.js
+++ script.js
@@ -2,5 +2,4 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
-console.log("Do NOT mess with any of the HTML doing so may break the site! :(");
-console.log("Btw the guy above me doesn't know js and uses discord bot maker")
+console.log("hi");
