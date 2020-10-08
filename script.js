c150ed05d2ec4af477bdd7a30dea3345ce778c2b
Glitch (airplanegobrr) <none>
2020-10-08 16:56:13 +0000

    Updated 9:55AM PST 10/8/2020

--- script.js
+++ script.js
@@ -2,5 +2,23 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
-console.log("Do NOT mess with any of the HTML doing so may break the site! :(");
-console.log("Btw the guy above me doesn't know js and uses discord bot maker")
+console.log("Mess with the code if you want, if you break something just refresh ¯\\_(ツ)_/¯ ");
+
+function sendEmail() {
+    var contact = document.getElementById("contactinfo").value;
+    var subjecttitle = document.getElementById("subject").value;
+    var inbodytext = document.getElementById("inbody").value;
+    var body = inbodytext + "<br><br><br>Contact info:" + contact;
+    Email.send({
+        SecureToken : "09111750-24e6-4094-a6b2-d977382eb0a9",
+        To : "airplanegobrrfeedback@gmail.com",
+        From : "flocknotif@gmail.com",
+        Subject : subjecttitle,
+        Body : body,
+    })
+    .then(console.log("Email sent"));
+}
+
+function disableb() {
+    document.getElementById("button").disabled = true;
+}
